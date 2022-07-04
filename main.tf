831d23c2a9f3d115b2714be1eb2c377e705eb3af
Edward Stone <ed@autotelic.com>
2022-07-04 21:36:50 +0100

    fix: local vars

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "cloudflare" {}
 locals {
   domain = "autotelic.dev"
   subdomain = "something"
+  zone_id = "14bfa107a70e0f04eca5b82d13da7263"
 }
 
 resource "fly_app" "exampleApp" {
@@ -14,21 +15,21 @@ resource "fly_app" "exampleApp" {
 
 resource "fly_cert" "exampleCert" {
   app      = fly_app.exampleApp.name
-  hostname = "${subdomain}.${domain}"
+  hostname = "${local.subdomain}.${local.domain}"
 }
 
 resource "cloudflare_record" "exampleCertDNSValidation" {
-  zone_id = "14bfa107a70e0f04eca5b82d13da7263"
-  name    = replace(fly_cert.exampleCert.dnsvalidationhostname, ".${domain}", "")
+  zone_id = local.zone_id
+  name    = replace(fly_cert.exampleCert.dnsvalidationhostname, ".${local.domain}", "")
   value   = fly_cert.exampleCert.dnsvalidationtarget
   type    = "CNAME"
   ttl     = 60
 }
 
 resource "cloudflare_record" "exampleAppSubDomain" {
-  zone_id = "14bfa107a70e0f04eca5b82d13da7263"
-  name    = "${fly_app.exampleApp.name}.fly.dev"
-  value   = fly_cert.exampleCert.hostname
+  zone_id = local.zone_id
+  name    = local.subdomain
+  value   = "${fly_app.exampleApp.name}.fly.dev"
   type    = "CNAME"
-  ttl     = 60
+  proxied = true
 }
